a62939af72d6a342082347ba4366b9448c06b53f
Artem Struk <giverva@gmail.com>
2019-07-31 10:44:39 +0700

    Correction Calculator.java

--- lesson_1/Calculator.java
+++ lesson_1/Calculator.java
@@ -1,44 +1,26 @@
 public class Calculator {
 	public static void main(String[] args) {
+		
+		double x = 2;
+		double y = 0;
+		char operation = '^' ;
 
-		//Реализуйте Калькулятор, который должен уметь выполнять математические операции
-		// (+, -, *, /, ^, %) над целыми положительными числами:
- 		// для проверки знака математической операции воспользуйтесь оператором if else
-    	// выведите на экран результат работы калькулятора с помощью System.out.println()
-
-		double x, y;
-		char op;
-		x = 2;
-		y = -3;
-		op = '^';
-
-		if(op == '+') {
+		if(operation == '+') {
 			System.out.println("Сумма чисел x + y = " + (x + y));
-		} else if(op == '-') {
+		} else if(operation == '-') {
 			System.out.println("Разность чисел x - y = " + (x - y));
-		} else if(op == '*') {
+		} else if(operation == '*') {
 			System.out.println("Умножение чисел x * y = " + (x * y));
-		} else if(op == '/') {
+		} else if(operation == '/') {
 			System.out.println("Деление чесел y / x = " + (y / x));
-		} else if(op == '%') {
+		} else if(operation == '%') {
 			System.out.println("Остаток от деления y % x = " + (y % x));
-		} else if(op == '^') {
-
+		} else if(operation == '^') {
 			double result = 1;
-			if (y > 0) {
-				for (int i = 1; i <= y; i++) {
-					result = result*x;
-				}				
-			} else if (y == 0) {
-				result = 1;
-			} else if (y < 0) {
-				for (int i = -1; i >= y; i--) {
-					result = result*x;				
-				}
-				result = 1 / result;
-			}
-
-			System.out.println("Возведение в степень x ^ y = " + result);	 
+			for (int i = 1; i <= y; i++) {
+				result = result*x;
+			}				
+		System.out.println("Возведение в степень x ^ y = " + result);	 
 		}
 	}
 }
