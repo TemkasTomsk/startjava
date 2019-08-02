1ca1f05d4df48d72c96f225bc4c3b80e2c1e46dc
Artem Struk <temkastomsk@gmail.com>
2019-08-02 22:50:31 +0700

    Implement HW2 class Person

---
+++ lesson_2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+	public static void main(String[] args) {
+		String gender = "Male";
+		String name = "Artem";
+		int height = 188;
+		float weight = 90.3f;
+		int age = 32;
+
+		void toWalk()  {
+
+		}
+
+		void sit() {
+
+		}
+
+		void run() {
+
+		}
+
+		void speak() {
+
+		}
+
+		void learnJava() {
+
+		}
+	}
+}
