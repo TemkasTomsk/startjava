5e0d077db06ea974ac99831824a77fd94ec88478
Artem Struk <giverva@gmail.com>
2019-08-06 17:41:58 +0700

    Edit JaegerTest

--- lesson_2/JaegerTest.java
+++ lesson_2/JaegerTest.java
@@ -1,35 +1,35 @@
 public class JaegerTest {
 
 	public static void main(String[] args) {
-		Jaeger jaegerGispyDanger = new Jaeger("Gispy Danger", "Mark-6", "USA", 79.25f, 1.980f, 7, 6);
+		Jaeger gispyDanger = new Jaeger("Gispy Danger", "Mark-6", "USA", 79.25f, 1.980f, 7, 6);
 
-		System.out.println(jaegerGispyDanger.getModelName());
-		System.out.println(jaegerGispyDanger.getMark());
-		System.out.println(jaegerGispyDanger.getCountry());
-		System.out.println(jaegerGispyDanger.getHeight());
-		System.out.println(jaegerGispyDanger.getWeight());
-		System.out.println(jaegerGispyDanger.getSpeed());
-		System.out.println(jaegerGispyDanger.getArmor());
+		System.out.println(gispyDanger.getModelName());
+		System.out.println(gispyDanger.getMark());
+		System.out.println(gispyDanger.getCountry());
+		System.out.println(gispyDanger.getHeight());
+		System.out.println(gispyDanger.getWeight());
+		System.out.println(gispyDanger.getSpeed());
+		System.out.println(gispyDanger.getArmor());
 
-		jaegerGispyDanger.move();
-		jaegerGispyDanger.drift();
-		jaegerGispyDanger.scanKaiju();
-		jaegerGispyDanger.useVortexCannon();
+		gispyDanger.move();
+		gispyDanger.drift();
+		gispyDanger.scanKaiju();
+		gispyDanger.useVortexCannon();
 		System.out.println("");
 
-		Jaeger jaegerChernoAlpha = new Jaeger("Cherno Alpha", "Mark-1", "RUSSIA", 85.34f, 2.412f, 3, 10);
+		Jaeger chernoAlpha = new Jaeger("Cherno Alpha", "Mark-1", "RUSSIA", 85.34f, 2.412f, 3, 10);
 
-		System.out.println(jaegerChernoAlpha.getModelName());
-		System.out.println(jaegerChernoAlpha.getMark());
-		System.out.println(jaegerChernoAlpha.getCountry());
-		System.out.println(jaegerChernoAlpha.getHeight());
-		System.out.println(jaegerChernoAlpha.getWeight());
-		System.out.println(jaegerChernoAlpha.getSpeed());
-		System.out.println(jaegerChernoAlpha.getArmor());
+		System.out.println(chernoAlpha.getModelName());
+		System.out.println(chernoAlpha.getMark());
+		System.out.println(chernoAlpha.getCountry());
+		System.out.println(chernoAlpha.getHeight());
+		System.out.println(chernoAlpha.getWeight());
+		System.out.println(chernoAlpha.getSpeed());
+		System.out.println(chernoAlpha.getArmor());
 
-		jaegerChernoAlpha.move();
-		jaegerChernoAlpha.drift();
-		jaegerChernoAlpha.scanKaiju();
-		jaegerChernoAlpha.useVortexCannon();
+		chernoAlpha.move();
+		chernoAlpha.drift();
+		chernoAlpha.scanKaiju();
+		chernoAlpha.useVortexCannon();
 	}
 }
