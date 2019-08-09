222489a36b605f5fbd9fd77fb55f6e87a342c837
Artem Struk <giverva@gmail.com>
2019-08-09 11:52:29 +0700

    Edit GuessNumber

--- lesson_2/GuessNumber.java
+++ lesson_2/GuessNumber.java
@@ -11,18 +11,18 @@ public class GuessNumber {
 	}
 		
 	public void startGame() {	
-		int uknownNumber = (int) (Math.random() * 101);
-		System.out.println("Загадано число от 0 до 100. Начинайте угадывать ");
+		int uknownNumber = (int) (Math.random() * 3);
+		System.out.println("Загадано число от 0 до 100. Начинайте угадывать.");
 
 		do {
-			System.out.println(firstPlayer.getName() + " Введите число:");
+			System.out.println(firstPlayer.getName() + " введите число:");
 			firstPlayer.setNumber(scan.nextInt());
 			if (firstPlayer.getNumber() > uknownNumber) {
 				System.out.println("Введенное вами число больше того, что загадал компьютер, введите снова: ");
 			} else if (firstPlayer.getNumber() < uknownNumber) {
 				System.out.println("Введенное вами число меньше того, что загадал компьютер, введите снова: ");
-			} else if (firstPlayer.getNumber() == uknownNumber) {
-				System.out.println(firstPlayer.getName() + " " + " Вы выиграли!");
+			} else {
+				System.out.println("Вы выиграли! " + firstPlayer.getName());
 				break;
 			}
 			System.out.println(secondPlayer.getName() + " Введите число:");
@@ -31,8 +31,8 @@ public class GuessNumber {
 				System.out.println("Введенное вами число меньше того, что загадал компьютер, введите снова: ");
 			} else if (secondPlayer.getNumber() > uknownNumber) {
 				System.out.println("Введенное вами число больше того, что загадал компьютер, введите снова: ");
-			} else if (secondPlayer.getNumber() == uknownNumber) {
-				System.out.println(firstPlayer.getName() + " Вы выиграли!");
+			} else {
+				System.out.println("Вы выиграли! " + secondPlayer.getName());
 				break;
 			} 
 		} while (true);
